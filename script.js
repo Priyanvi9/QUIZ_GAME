434d6aef716a8ad4da195a3247bbdfa5d0f7f0a4
PRIYANVI GUPTA <priyanvigupta869@gmail.com>
2024-11-16 13:53:42 +0530

    Add files via upload
    Uploading files

---
+++ script.js
@@ -0,0 +1,33 @@
+function askName() {
+  const name = prompt("Please enter your name : ");
+  const messageElement = document.getElementById("message");
+  if (name) {
+    messageElement.innerHTML = `Welcome, <strong>${name}</strong>! Test your abilities`;
+  } else {
+    messageElement.innerHTML = `NA NA Nam to dalna pdega, <br> no worry fill the name again and see magic`;
+  }
+}
+{
+  /* <div class="custom-alert" id="customConfirm" style="display: none;">
+  <p>Are you sure you want to proceed?</p>
+  <button onclick="confirmAction(true)">Yes</button>
+  <button onclick="confirmAction(false)">No</button>
+</div>
+
+<script>
+  function showCustomConfirm() {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("customConfirm").style.display = "block";
+  }
+
+  function confirmAction(result) {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("customConfirm").style.display = "none";
+    if (result) {
+      console.log("User clicked Yes");
+    } else {
+      console.log("User clicked No");
+    }
+  }
+</script> */
+}
---
+++ script2.js
@@ -0,0 +1,1 @@
+document.getElementsByClassName("subject").onclick = function () {};
