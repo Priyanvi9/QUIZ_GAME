b2653213503466bf65339d88abd9466bfbc4d8a3
PRIYANVI GUPTA <priyanvigupta869@gmail.com>
2024-11-16 22:00:19 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-document.getElementsByClassName("subject").onclick = function () {};
-=======
-document.getElementsByClassName("subject").onclick = function () {};
->>>>>>> ed6de47abcf6189c01c835b4446180a4c2d4a30e
+// <<<<<<< HEAD
+// document.getElementsByClassName("subject").onclick = function () {};
+// =======
+// document.getElementsByClassName("subject").onclick = function () {};
+// >>>>>>> ed6de47abcf6189c01c835b4446180a4c2d4a30e
